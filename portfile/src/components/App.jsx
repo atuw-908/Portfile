44a81ffc50796c36edac34b7b87f7ede9e62a267
unknown <orifjonkurbonov4@gmail.com>
2024-02-06 20:04:22 +0500

    add initial files

---
+++ portfile/src/components/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import Main from "./main"
+
+function App() {
+
+  return (
+  <>
+  <Header/>
+  <Main/>
+  <Footer/>
+  </>
+  )
+}
+
+export default App
